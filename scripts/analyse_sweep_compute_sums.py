a5a0a40ac5b422a6867e13b74ca087f2c043a512
mikkel <lep.mik@gmail.com>
2021-11-04 20:53:42 +0100

    analyze sweep by computing sums from each rank

---
+++ scripts/analyse_sweep_compute_sums.py
@@ -0,0 +1,129 @@
+import numpy as np
+from tqdm import tqdm
+import pandas as pd
+import pathlib
+from collections import defaultdict
+from scipy.linalg import svd
+from functools import partial
+import ruamel.yaml
+
+from causal_optoconnectics.tools import (
+    compute_trials_multi,
+    decompress_events,
+    error,
+    error_norm,
+    min_error,
+    process,
+    process_metadata
+)
+
+rparams = {
+    'x1': 11,
+    'x2': 13,
+    'y1': 12,
+    'y2': 19,
+    'z1': 7,
+    'z2': 10,
+}
+
+def conditional_sum(s1, s2):
+    l = [
+        'yz_sum',
+        'z_sum',
+        'yx_sum',
+        'x_sum',
+        'yxinv_sum',
+        'xinv_sum',
+        'y0z_sum',
+        'y0x_sum',
+        'y0xinv_sum',
+        'n_trials'
+    ]
+    if s1.name in l:
+        return s1 + s2
+    else:
+        assert (s1 == s2).all()
+        return s1
+
+def compute_connectivity_from_sum(row, params=None):
+    conn = Connectivity(
+        compute_values=False,
+        compute_sums=False
+    )
+    conn.__dict__.update(row.to_dict())
+    conn.compute()
+    result = conn.__dict__
+    if params is not None:
+        result.update(params)
+    return result
+
+if __name__ == '__main__':
+    import sys
+    from functools import reduce
+    yaml = ruamel.yaml.YAML()
+    data_path = pathlib.Path(sys.argv[1]).absolute().resolve()
+    print(f'Analyzing {data_path}')
+    paths = [path for path in data_path.iterdir() if path.is_dir()]
+    data_df = pd.DataFrame({'path': paths})
+
+    values = pd.DataFrame()
+    for i, row in tqdm(data_df.iterrows(), total=len(data_df)):
+        #print(f'Working on {row.path}')
+        samples = []
+        for fn in row.path.glob('*.npz'):
+            if fn.stem == 'trials':
+                continue
+            data = np.load(fn, allow_pickle=True)
+            data = {k: data[k][()] for k in data.keys()}
+            X = data['data']
+            W_0 = data['W_0']
+            W = data['W']
+            stim_index = len(W_0)
+            params = data['params']
+            params.update(rparams)
+            data_df.loc[i, params.keys()] = params.values()
+            data_df.loc[i, 'sigma'] = params['glorot_normal']['sigma']
+
+
+            results_meta = pd.DataFrame(process_metadata(W=W, stim_index=stim_index, params=params))
+            sample_meta = results_meta.query('source_stim and not target_stim and weight >= 0')
+            neurons = pd.concat((sample_meta.source, sample_meta.target)).unique()
+            trials = compute_trials_multi(X, neurons, stim_index)
+            sums = pd.DataFrame([
+                process(pair=pair, W=W, stim_index=stim_index, trials=trials, params=None, compute_values=False, compute_sums=True)
+                for pair in sample_meta.pair.values])
+            sums.to_csv(fn.with_suffix('.csv'))
+            samples.append(sums)
+
+        with open(row.path / 'params.yaml', 'w') as f:
+            yaml.dump(params, f)
+        n_neurons = params['n_neurons']
+
+        s_W = svd(W_0, compute_uv=False)
+        data_df.loc[i, 'W_condition'] = s_W.max() / s_W.min()
+        data_df.loc[i, 'W_smin'] = s_W.min()
+        data_df.loc[i, 'W_smax'] = s_W.max()
+
+        x = decompress_events(X, len(W), params['n_time_step'])
+        cov_x = np.cov(x[:len(W_0)])
+        s_cov = svd(cov_x, compute_uv=False)
+        data_df.loc[i, 'cov_condition'] = s_cov.max() / s_cov.min()
+        data_df.loc[i, 'cov_smin'] = s_cov.min()
+        data_df.loc[i, 'cov_smax'] = s_cov.max()
+
+
+        sample = reduce(lambda  left, right: pd.combine(left, right, conditional_sum), samples)
+        sample = pd.DataFrame([
+            compute_connectivity_from_sum(row, params=params)
+            for i, row in sample.iterrows()])
+        sample.to_csv(row.path / 'sample.csv')
+        values = pd.concat((values, sample))
+        data_df.loc[i, 'error_beta_did'] = min_error(sample, 'beta_did').fun
+        data_df.loc[i, 'error_beta_iv_did'] = min_error(sample, 'beta_iv_did').fun
+        data_df.loc[i, 'error_beta'] = min_error(sample, 'beta').fun
+        data_df.loc[i, 'error_beta_iv'] = min_error(sample, 'beta_iv').fun
+
+    data_df.loc[:,'error_diff'] = data_df.error_beta - data_df.error_beta_iv
+    data_df.loc[:,'error_diff_did'] = data_df.error_beta_did - data_df.error_beta_iv_did
+    data_df.to_csv(data_path / 'summary.csv')
+    values.to_csv(data_path / 'values.csv')
