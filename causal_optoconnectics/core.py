b41ddac57bf1e4e91fa51d9f2cdd2b9c6d3eddfa
Mikkel Lepperød <lep.mik@gmail.com>
2021-11-04 11:11:51 +0000

    proper baseline for DiD

--- causal_optoconnectics/core.py
+++ causal_optoconnectics/core.py
@@ -81,38 +81,44 @@ def find_response_spikes(x, y, s, z1, z2, dt):
 class Connectivity:
     def __init__(self, pre, post, x1, x2, y1, y2, z1, z2):
         '''
-        pre/post are trials of size N_trials x N_bins
-
+        pre/post are trials of size n_trials x n_bins
+        assumes that the stimulation starts on the middle bin
         x1, x2, y1, y2, z1, z2 are bin indices for where
         '''
+        n_bins = pre.shape[1]
+        assert n_bins % 2 == 0
+        #stim_idx = int(n_bins / 2)
+        n_response = y2-y1
 
         x = pre[:, x1:x2].sum(1).astype(bool)
         y = post[:, y1:y2].sum(1).astype(bool)
         z = pre[:, z1:z2].sum(1).astype(bool)
-        y0 = post[:, 0:y2-y1].sum(1).astype(bool)
+
+        #y0 = post[:, stim_idx-n_response:stim_idx].sum(1).astype(bool)
+        y0 = post[:,y1-n_response:y2-n_response].sum(1).astype(bool)
 
         self.x, self.y, self.z, self.y0 = x, y, z, y0
 
         y_refractory = (y*z).sum() / z.sum()
 
-        y_respons = (y*x).sum() / x.sum()
+        y_response = (y*x).sum() / x.sum()
 
         y_nospike = (y*(1-x)).sum() / (1-x).sum()
 
         y0_refractory = (y0*z).sum() / z.sum()
 
-        y0_respons = (y0*x).sum() / x.sum()
+        y0_response = (y0*x).sum() / x.sum()
 
         y0_nospike = (y0*(1-x)).sum() / (1-x).sum()
 
         # standard iv
-        self.beta_iv = y_respons - y_refractory
+        self.beta_iv = y_response - y_refractory
         # OLS
-        self.beta = y_respons - y_nospike
+        self.beta = y_response - y_nospike
 
         # DiD iv
-        self.beta_iv_did = self.beta_iv - (y0_respons - y0_refractory)
+        self.beta_iv_did = self.beta_iv - (y0_response - y0_refractory)
         # OLS
-        self.beta_did = self.beta - (y0_respons - y0_nospike)
+        self.beta_did = self.beta - (y0_response - y0_nospike)
 
         self.hit_rate = x.mean()
